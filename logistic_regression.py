4b8bb8953faf05cab59e6b532475845792d1f5ed
sindhusita <sindhusita.11@gmail.com>
2026-09-02 20:44:01 -0700

    plot adjust

--- logistic_regression.py
+++ logistic_regression.py
@@ -58,6 +58,7 @@ if __name__ == "__main__":
         print("Accuracy:iter: ", accuracy, iter)
     
     fig = plt.plot(iters, accuracies)
+    plt.title("No. of iterations vs Accuracy:")
     plt.show()
 
 
