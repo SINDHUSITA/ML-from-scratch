bb622146da61908666eb08626fc814e85514dda2
sindhusita <sindhusita.11@gmail.com>
2026-09-18 16:19:42 -0700

    add manhattan distance

--- kmeans_with_loops.py
+++ kmeans_with_loops.py
@@ -9,14 +9,16 @@ def assign_clusters(cluster_assignments, centroids, points):
     for i, p in enumerate(points):
         distances = []
         for centroid in centroids:
-            distance = get_eu_distance(p, centroid)
+            distance = get_distance(p, centroid,norm=distance_metric)
             distances.append(distance)
         cluster_assignments[i] = np.argmin(distances)
 
     return cluster_assignments
 
-def get_eu_distance(a, b):
-        return np.sqrt(sum((a-b)**2))
+def get_distance(a, b, norm = 2):
+    # return np.sqrt(sum((a-b)**2))
+    # ord=1 for L1 Norm and Manhattan dist, ord=2 for L2 norm or Euclidian dist
+    return np.linalg.norm(a - b, ord=norm)
 
                           
 if __name__ == "__main__":
@@ -24,6 +26,7 @@ if __name__ == "__main__":
     n_features = 5
     k = 5
     max_iters = 100
+    distance_metric = 2 # 1 for Manhattan distance
 
     points = np.random.randn(n_samples, n_features)
     cluster_assignments = np.zeros(n_samples, dtype=int)
@@ -31,14 +34,14 @@ if __name__ == "__main__":
     centroids = points[init_centroid_indices]
 
     max_dist = 0
-    dist_from_centroid = [get_eu_distance(centroids[c],p) for c,p in zip(cluster_assignments, points)]
+    dist_from_centroid = [get_distance(centroids[c],p,norm=distance_metric) for c,p in zip(cluster_assignments, points)]
     distances = []
     for t in range(max_iters):
         
         cluster_assignments = assign_clusters(cluster_assignments, centroids, points)
         new_centroids = get_centroid(points, cluster_assignments,k)
         
-        dist_from_centroid = [get_eu_distance(new_centroids[c],p) for c,p in zip(cluster_assignments, points)]
+        dist_from_centroid = [get_distance(new_centroids[c],p, norm=distance_metric) for c,p in zip(cluster_assignments, points)]
         print(f"At t={t}, dist_from_centroid", sum(dist_from_centroid))  
         distances.append(sum(dist_from_centroid))
         if np.all(new_centroids == centroids):
