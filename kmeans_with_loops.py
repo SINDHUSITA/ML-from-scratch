a7e852c45ef0705e14d04595b6741ecd55f1e3b2
sindhusita <sindhusita.11@gmail.com>
2026-09-18 15:24:32 -0700

    kmeans with loops

---
+++ kmeans_with_loops.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_centroid(points, cluster_assignments,k):
+        return np.array([np.mean(points[cluster_assignments == i], axis=0) for i in range(k)])
+              
+def assign_clusters(cluster_assignments, centroids, points):
+
+    for i, p in enumerate(points):
+        distances = []
+        for centroid in centroids:
+            distance = get_eu_distance(p, centroid)
+            distances.append(distance)
+        cluster_assignments[i] = np.argmin(distances)
+
+    return cluster_assignments
+
+def get_eu_distance(a, b):
+        return np.sqrt(sum((a-b)**2))
+
+                          
+if __name__ == "__main__":
+    n_samples = 100
+    n_features = 5
+    k = 2
+    max_iters = 100
+
+    points = np.random.randn(n_samples, n_features)
+    cluster_assignments = np.zeros(n_samples, dtype=int)
+    init_centroid_indices = np.random.choice(n_samples, size=k, replace=False)
+    centroids = points[init_centroid_indices]
+
+    max_dist = 0
+    dist_from_centroid = [get_eu_distance(centroids[c],p) for c,p in zip(cluster_assignments, points)]
+    distances = []
+    for t in range(max_iters):
+        
+        cluster_assignments = assign_clusters(cluster_assignments, centroids, points)
+        new_centroids = get_centroid(points, cluster_assignments,k)
+        
+        dist_from_centroid = [get_eu_distance(new_centroids[c],p) for c,p in zip(cluster_assignments, points)]
+        print(f"At t={t}, dist_from_centroid", sum(dist_from_centroid))  
+        distances.append(sum(dist_from_centroid))
+        if np.all(new_centroids == centroids):
+             print("CONVERGENCE")
+             break
+        centroids = new_centroids
+    
+    
+    print("Final assignments: ", cluster_assignments)  
+    plt.plot(distances)
+    plt.xlabel("Iteration")
+    plt.ylabel("SSE")
+    plt.show()
+
+
+
+
